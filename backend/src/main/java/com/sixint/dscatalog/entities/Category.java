c286f474ee2f490031f65b359111afb5d7400515
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-07-21 23:18:22 -0300

    H2 Database

--- backend/src/main/java/com/sixint/dscatalog/entities/Category.java
+++ backend/src/main/java/com/sixint/dscatalog/entities/Category.java
@@ -5,19 +5,21 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Table_category")
+@Table(name = "tb_category")
 public class Category implements Serializable{
 /**
 * serialVersionUID applied to Category, allow java objects to converted in bytes. Automatic implementation.
 */
 	private static final long serialVersionUID = 1L;
 	
+
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 

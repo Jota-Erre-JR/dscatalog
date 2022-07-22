6edbf1090be9478468b4d3e72c09be294fb8f77a
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-07-21 23:42:12 -0300

    Database Seeding

--- backend/src/main/java/com/sixint/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/sixint/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sixint.dscatalog.entities.Category;
 import com.sixint.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
+	/*Transactional grant that transaction will be done inside the database and the readonly
+	 *  increase the performance, avoid the locking of database.
+	 */
 	public List<Category> findAll(){
 		return repository.findAll();
 	}

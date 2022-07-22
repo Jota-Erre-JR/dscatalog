4aec473af4f577f763ff055a178b996a96e74563
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-07-22 20:12:12 -0300

    Find category by id, exception handling

---
+++ backend/src/main/java/com/sixint/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.sixint.dscatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sixint.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource not found.");
+		err.setMessage("Review the item you typed.");
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+		
+	}
+}
+
--- backend/src/main/java/com/sixint/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/sixint/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.sixint.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +11,30 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sixint.dscatalog.dto.CategoryDTO;
 import com.sixint.dscatalog.entities.Category;
 import com.sixint.dscatalog.repositories.CategoryRepository;
+import com.sixint.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	@Transactional(readOnly = true)
-	/*Transactional grant that transaction will be done inside the database and the readonly
-	 *  increase the performance, avoid the locking of database.
-	 */
-	public List<CategoryDTO> findAll(){
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new  CategoryDTO(x)).collect(Collectors.toList());
 
-}
+	/*
+	 * Transactional grant that transaction will be done inside the database and the
+	 * readonly increase the performance, avoid the locking of database.
+	 */
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		return new CategoryDTO(entity);
+	
+	}
 }

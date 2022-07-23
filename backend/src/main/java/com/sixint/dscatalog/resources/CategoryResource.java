d483cb51f32a123b4e4962d62b17ed65cd16f2b2
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-07-23 18:56:05 -0300

    Created PUT, UPDATE and DELETE methods

--- backend/src/main/java/com/sixint/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/sixint/dscatalog/resources/CategoryResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,7 @@ public class CategoryResource {
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		CategoryDTO dto = service.findById(id);
@@ -44,4 +46,19 @@ public class CategoryResource {
 		return ResponseEntity.created(uri).body(dto);
 	}
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	/*
+	 * At delete is possible to use RespnseEntity<void>, instead
+	 * ResponseEntity<CategoryDTO>, because the body of answer is empty.
+	 */
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- backend/src/main/java/com/sixint/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/sixint/dscatalog/services/CategoryService.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sixint.dscatalog.dto.CategoryDTO;
 import com.sixint.dscatalog.entities.Category;
 import com.sixint.dscatalog.repositories.CategoryRepository;
+import com.sixint.dscatalog.services.exceptions.DataBaseException;
 import com.sixint.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -35,7 +40,7 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Please review the typed content."));
 		return new CategoryDTO(entity);
-	
+
 	}
 
 	@Transactional
@@ -44,5 +49,30 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);//At new Spring Boot versions getOne changed to getReferenceById().
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id " + id + " not found!");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id " + id + " not found!");
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation!");
+		}
 	}
 }

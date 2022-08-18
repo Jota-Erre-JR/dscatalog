85c8dbe3ac35b7cbb4f3e65b5c6865dfe1ce2e18
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-18 20:40:50 -0300

    Custom Validation Response

--- backend/src/main/java/com/sixint/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/sixint/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -34,8 +36,28 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
-		err.setError("Resource not found.");
+		err.setError("You cannot delete this item as it has dependencies.");
 		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation Exception.");
+		err.setMessage(e.getMessage());
+		
+
+		for (FieldError f : e.getBindingResult().getFieldErrors()){
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
 		err.setPath(request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		

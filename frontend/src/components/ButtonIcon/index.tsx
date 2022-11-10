8aaa9723eba51f981cb9dfc784c3711f8355c36e
Julio Correa <juliocesarjr@outlook.com>
2022-11-10 11:29:00 -0300

    Props

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,10 +1,16 @@
 import './styles.css';
+
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
-const ButtonIcon = () => {
+
+type Props = {
+  text: string;
+}
+
+const ButtonIcon = ({text} : Props) => {
   return (
     <div className="btn-container">
       <button className="btn btn-primary">
-        <h6>INICIE AGORA A SUA BUSCA</h6>
+        <h6>{text}</h6>
       </button>
       <div className="btn-icon-container">
         <ArrowIcon />
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ const Home = () => {
             </div>
             <div>
               <Link to="/products">
-                <ButtonIcon />
+                <ButtonIcon text="Inicie agora sua busca" />
               </Link>
             </div>
           </div>

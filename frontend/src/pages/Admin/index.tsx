403fe31af153e7bc22d5202ce668358a9a60083e
Julio Correa <juliocesarjr@outlook.com>
2022-11-15 01:10:17 -0300

    Admin Navbar layout mobile

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,6 +1,15 @@
+import Navbar from "./Navbar";
+import './styles.css';
+
 const Admin = () => {
   return (
-    <h1>Admin</h1>
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+
+      </div>
+
+    </div>
   );
 };
 

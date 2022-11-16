1bb45d7a0a10a9fd417c78d960b8c2a66ad76728
Julio Correa <juliocesarjr@outlook.com>
2022-11-15 21:08:12 -0300

    Routes, redirect, nesting

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,7 +3,7 @@ import Admin from 'pages/Admin';
 import Catalog from 'pages/Catalog';
 import Home from 'pages/Home';
 import ProductDetails from 'pages/ProductDetails';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 const Routes = () => {
   return (
@@ -19,6 +19,7 @@ const Routes = () => {
         <Route path="/products/:productId">
           <ProductDetails />
         </Route>
+        <Redirect from="/admin" to="/admin/products" exact />
         <Route path="/admin">
           <Admin />
         </Route>
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,3 +1,4 @@
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import './styles.css';
 
@@ -6,7 +7,17 @@ const Admin = () => {
     <div className="admin-container">
       <Navbar />
       <div className="admin-content">
-
+      <Switch>
+        <Route path="/admin/products">
+          <h1>Products CRUD</h1>
+        </Route>
+        <Route path="/admin/categories">
+          <h1>Categories CRUD</h1>
+        </Route>
+        <Route path="/admin/users">
+          <h1>Users CRUD</h1>
+        </Route>
+      </Switch>
       </div>
 
     </div>

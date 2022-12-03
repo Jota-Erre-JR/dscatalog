19ba0bfccd24172d107e19e7bd334718dc231b67
Julio Correa <juliocesarjr@outlook.com>
2022-12-02 23:21:52 -0300

    Fix AxiosRequestConfig

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
-import { AxiosParams } from 'types/vendor/axios';
+
 import { SpringPage } from 'types/vendor/spring';
 import { BASE_URL } from 'util/requests';
 import CardLoader from './CardLoader';
@@ -16,9 +16,10 @@ const Catalog = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const params: AxiosParams = {
+    const params: AxiosRequestConfig = {
       method: 'GET',
-      url: `${BASE_URL}/products`,
+      url: "/products",
+      baseURL: BASE_URL,
       params: {
         page: 0,
         size: 12,

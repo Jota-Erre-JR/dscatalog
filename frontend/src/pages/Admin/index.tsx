8aff71fc76ccc5b8d1c2c560fb53c5edc5a55fd1
Julio Correa <juliocesarjr@outlook.com>
2022-12-30 21:34:19 -0300

    PrivateRoute

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,7 +1,8 @@
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import './styles.css';
 import Users from "./User";
+import PrivateRoute from "components/PrivateRoute";
 
 const Admin = () => {
   return (
@@ -9,15 +10,15 @@ const Admin = () => {
       <Navbar />
       <div className="admin-content">
       <Switch>
-        <Route path="/admin/products">
+        <PrivateRoute path="/admin/products">
           <h1>Products CRUD</h1>
-        </Route>
-        <Route path="/admin/categories">
+        </PrivateRoute>
+        <PrivateRoute path="/admin/categories">
           <h1>Categories CRUD</h1>
-        </Route>
-        <Route path="/admin/users">
+        </PrivateRoute>
+        <PrivateRoute path="/admin/users">
           <Users />
-        </Route>
+        </PrivateRoute>
       </Switch>
       </div>
 

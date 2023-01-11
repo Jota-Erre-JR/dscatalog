03560e1b3890f2c181c19b5ad710ab805f13a678
Julio Correa <juliocesarjr@outlook.com>
2023-01-11 12:38:34 -0300

    Category badge

---
+++ frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -0,0 +1,31 @@
+import './styles.css';
+
+import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
+import CategoryBadge from '../CategoryBadge';
+
+type Props = {
+  product: Product;
+};
+const ProductCrudCard = ({ product }: Props) => {
+  return (
+    <div className="base-card product-crud-card">
+      <div className="card-crud-top-container">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div>
+        <div className="card-crud-bottom-container">
+          <h6>{product.name}</h6>
+          <ProductPrice price={product.price} />
+        </div>
+        <div className="product-crud-categories-badges">
+          {product.categories.map((category) => (
+            <CategoryBadge name={category.name} key={category.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCrudCard;

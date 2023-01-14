bdbe82d5a2107cb4853a4da30c4994b2c2271c25
Julio Correa <juliocesarjr@outlook.com>
2023-01-14 20:40:19 -0300

    Product CRUD Form HTML

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,10 +1,29 @@
-
 import './styles.css';
 
 const Form = () => {
   return (
     <>
-      <div>Form</div>
+      <div className="product-crud-container">
+        <div className="base-card product-crud-form-card">
+          <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+          <form>
+            <div className="row">
+              <div className="col-lg-6">
+                <input type="text" className="base-input form-control"/>
+                <input type="text" className="base-input form-control"/>
+                <input type="text" className="base-input form-control"/>
+              </div>
+              <div className="col-lg-6">
+                <textarea name="" rows={10} className="base-input form-control"></textarea>
+              </div>
+            </div>
+            <div className="row">
+              <button className="btn btn-cancel">CANCELAR</button>
+              <button className="btn btn-save">SALVAR</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };

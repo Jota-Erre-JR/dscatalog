64824f5f6c8bd0dabbbe889613c3dde117ce04e7
Julio Correa <juliocesarjr@outlook.com>
2023-01-18 23:04:38 -0300

    Form buttons styling

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -7,17 +7,34 @@ const Form = () => {
         <div className="base-card product-crud-form-card">
           <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
           <form>
-            <div className="row">
-              <div className="col-lg-6">
-                <input type="text" className="base-input form-control"/>
-                <input type="text" className="base-input form-control"/>
-                <input type="text" className="base-input form-control"/>
+            <div className="row product-crud-input-container">
+              <div className="col-lg-6 product-crud-input-left-container">
+                <div className="margin-bottom-30">
+                  <input type="text" className="base-input form-control" />
+                </div>
+                <div className="margin-bottom-30">
+                  <input
+                    type="text"
+                    list="genre"
+                    className="base-input form-control"
+                  />
+                  <datalist id="genre"></datalist>
+                </div>
+                <div>
+                  <input type="text" className="base-input form-control" />
+                </div>
               </div>
               <div className="col-lg-6">
-                <textarea name="" rows={10} className="base-input form-control"></textarea>
+                <div>
+                  <textarea
+                    name=""
+                    rows={10}
+                    className="form-control base-input h-auto"
+                  ></textarea>
+                </div>
               </div>
             </div>
-            <div className="row">
+            <div className="product-crud-buttons-container">
               <button className="btn btn-cancel">CANCELAR</button>
               <button className="btn btn-save">SALVAR</button>
             </div>

a51c976995eb4d910132ae4f0f59ec6694d423a8
Julio Correa <juliocesarjr@outlook.com>
2023-01-19 21:18:41 -0300

    Product list from backend

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,32 +1,29 @@
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Product } from 'types/product';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 const List = () => {
-  const product = {
-    id: 1,
-    name: 'The Lord of the Rings',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 90.5,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
-    date: '2020-07-13T20:50:07.123450Z',
-    categories: [
-      {
-        id: 2,
-        name: 'Eletrônicos',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
       },
-      {
-        id: 3,
-        name: 'Livros',
-      },
-      {
-        id: 1,
-        name: 'Computadores',
-      },
-    ],
-  };
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
     <>
@@ -39,18 +36,11 @@ const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div key={product.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </>
   );
